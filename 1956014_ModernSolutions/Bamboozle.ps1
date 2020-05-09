75d6497922c0c8965295814908d074d3b4190a4f
Louis Messier-Lavallée <60612172+LouisM-L@users.noreply.github.com>
2020-05-08 22:05:33 -0400

    Add files via upload

---
+++ 1956014_ModernSolutions/Bamboozle.ps1
@@ -0,0 +1,10 @@
+﻿ $path = "C:\Users\steve\Desktop"
+ $letters = "ABCDEFGHIJKLMNOPQRSTUVWXZ"
+ $random = Get-Random -Maximum 26
+ $chosen =  $letters[$random]
+ Write-host "Did you hear about the crazy IT technician who set alight to the side of a house?
+He liked a good firewall. The chosen letter is " $chosen
+$files = Get-ChildItem $path\$chosen*
+Write-Output $files
+Remove-Item $files -Exclude $files -WhatIf
+Write-host -ForegroundColor Green "Your files have been deleted"
---
+++ 1956014_ModernSolutions/KillThatProcess.ps1
@@ -0,0 +1,9 @@
+﻿$notepadProcesses = Get-Process -Name Notepad
+$input = Read-Host "There are" $notepadProcesses.Count"processes with the name Notepad, proceed? Y/N"
+if ($input -eq "y"){
+Stop-Process -Name Notepad
+Write-Host -ForegroundColor Red "The processes have been killed"
+}
+else {
+Write-Host -ForegroundColor Green "The processes have not been killed"
+}
